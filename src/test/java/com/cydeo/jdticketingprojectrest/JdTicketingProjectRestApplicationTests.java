e0dd356ec968d97679a23699404684467e7a1d74
aissakhova@gmail.com <aktoty@live.com>
2023-07-19 10:54:08 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/jdticketingprojectrest/JdTicketingProjectRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.jdticketingprojectrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdTicketingProjectRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
